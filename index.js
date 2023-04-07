40fcafa53c9efbd522fc2a1257eb9fa1bb2e6e38
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-04-07 19:24:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const myName = "Etsegenet Alamerew";
+const myAge = 30;
+const coder = true;
+const message = "Hello, my name is "  +  myName  +  " I am " + myAge + " Years old and I can code JavaScript:" + "";
+console.log(message);
