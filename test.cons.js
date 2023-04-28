d2580ab8ce791f3f84632868dadf01bdb88951ad
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-04-28 12:16:56 -0700

    Add files via upload

---
+++ test.cons.js
@@ -0,0 +1,8 @@
+const people = { friends: [] };
+const friend1 = {firstName: "Alice", lastName: "Smith", id: 1234};
+const friend2 = {firstName: "Bob", lastName: "Jones", id: 5678};
+const friend3 = {firstName: "Charlie", lastName: "Brown", id: 9012};
+people.friends.push(friend1, friend2, friend3);
+console.log(people.friends);
+
+
